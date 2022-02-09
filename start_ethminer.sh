f4b82a4483cfba1673ecc7b8dd3d5e3fb53394ac
a5100996 <a5100996@gmail.com>
2022-02-09 18:36:25 +0800

    出版

---
+++ start_ethminer.sh
@@ -0,0 +1,4 @@
+#
+# Example shell file for starting ethminer
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+./ethminer/ethminer -P stratum1+tcp://0x3a0ae474ebf79ed9e489a778c0530708fe1fd691.Rig001@eth-eu1.nanopool.org:9999
---
+++ start_phoenixminer.sh
@@ -0,0 +1,5 @@
+#
+# Example shell file for starting PhoenixMiner to mine ETH
+#
+# IMPORTANT: Replace the ETH address with your own ETH wallet address in the -wal option (Rig001 is the name of the rig)
+./phoenixminer/PhoenixMiner -pool eth-eu1.nanopool.org:9999 -wal 0x3a0ae474ebf79ed9e489a778c0530708fe1fd691/Rig001 -pass x
